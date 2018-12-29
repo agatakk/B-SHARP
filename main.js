78161223d7958cc986595c075d4f12a7ceda5260
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2018-12-29 15:42:17 +0100

    HTML - sekcja #green bez div; CSS - animacja #green; jQuery - animacja borderRadius i margin minus sekcji #green

---
+++ main.js
@@ -0,0 +1,11 @@
+setTimeout(function(){$("#green").animate({
+    width: '100vw',
+    height: '100vh',
+    top: "0",
+    left:"0",
+    borderRadius:'0'
+},1500)}, 5000);
+setTimeout(function(){$("#par").animate({
+    padding: '20px',
+    marginLeft: '-800px',
+  },1500)},5000);
