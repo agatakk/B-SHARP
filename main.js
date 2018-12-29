9e28fbc062e1b4dbf3dc639a85cd7b9af7afb4e4
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2018-12-29 17:25:31 +0100

    Typewriter effect sekcji #text za pomocą jQuery

--- main.js
+++ main.js
@@ -9,3 +9,33 @@ setTimeout(function(){$("#par").animate({
     padding: '20px',
     marginLeft: '-800px',
   },1500)},5000);
+
+  function changeText(cont1,cont2,speed){
+          var Otext=cont1.text();
+          var Ocontent=Otext.split("");
+          var i=0;
+          function show(){
+            if(i<Ocontent.length){  
+                cont2.append(Ocontent[i]);
+                 i=i+1;
+          };
+        };
+           var Otimer=setInterval(show,speed);
+        };
+   setTimeout(function(){
+          changeText($(".intro"),$(".p2"),40); //  150 = the Delay time in milliseconds between strokes.
+          clearInterval(Otimer);
+        },6500);
+
+    setTimeout(function(){
+            changeText($(".coming"),$(".p3"),50); //  150 = the Delay time in milliseconds between strokes.
+            clearInterval(Otimer);
+          },11300);
+
+    setTimeout(function(){
+            changeText($(".sign-up"),$(".p4"),45); //  150 = the Delay time in milliseconds between strokes.
+            clearInterval(Otimer);
+          },12000);
+
+    
+    
