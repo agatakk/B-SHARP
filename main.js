01aa4b934e3106904a964a187fc0953649e54107
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2019-01-07 08:33:46 +0100

    Animacja margin-left - zamiana jQuery na css

--- main.js
+++ main.js
@@ -8,20 +8,80 @@ setTimeout(function(){$("#green").animate({
 
 
 
-setTimeout(function(){$("#par").animate({
-    padding: '20px',
-    marginLeft: '-90vh',
-  },1500)},5000);
+// setTimeout(function(){$("#par").animate({
+//     padding: '20px',
+//     // marginLeft: '-90vh',
+//   },1500)},5000);
 
 
-let viewportMobile = window.matchMedia("screen and (max-width: 767px");
 
-if(viewportMobile.matches){
+// const mql = window.matchMedia("screen and (max-width: 1024px)");
+
+// const mql1 =  window.matchMedia('screen and (max-width: 768px)');
+
+// function moveLogo (mql){
+//   if (mql.matches){
+//     setTimeout(function(){$("#par").animate({
+//          marginLeft: '-40vh',
+          
+//      },1050)},5000);
+//      console.log("działa");
+//   }
+//   else if (mql1.matches){
+//     setTimeout(function(){$("#par").animate({
+//          marginLeft: '-350vh',
+//            },1450)},5000);
+//   }
+//   else{
+//     setTimeout(function(){$("#par").animate({
+//       padding: '20px',
+//       marginLeft: '-90vh',
+//     },1500)},5000);
+//   }
+// }
+// moveLogo(mql);
+// mql.addListener(moveLogo);
+// mql1.addListener(moveLogo);
+  
+// let viewportMobile = window.matchMedia("screen and (max-width: 767px");
+// let viewportMobileLarge = window.matchMedia("screen and (max-width: 1024px");
+
+// if(viewportMobile.matches){
+//     setTimeout(function(){$("#par").animate({
+//         marginLeft: '-350vh',
+//       },1450)},5000);
+// }
+
+// if(viewportMobileLarge.matches){
+//   setTimeout(function(){$("#par").animate({
+//     marginLeft: '-40vh',
+    
+//   },1050)},5000);
+// }
+const mql = window.matchMedia('screen and (max-width: 1024px)')
+
+const mqls = [
+  window.matchMedia("(max-width: 1024px)"),
+  window.matchMedia("(max-width: 768px)"),
+]
+ function moveLogo(mql){
+   if(mqls[0].matches){
     setTimeout(function(){$("#par").animate({
-        marginLeft: '-350vh',
+       marginLeft: '-40vh',
+         },1050)},5000);
+   }
+   else if(mqls[1].matches){
+    setTimeout(function(){$("#par").animate({
+         marginLeft: '-350vh',
       },1450)},5000);
-}
-
+   }
+   else{
+    setTimeout(function(){$("#par").animate({
+         padding: '20px',
+          marginLeft: '-90vh',
+      },1500)},5000);
+   }
+ }
 
   function changeText(cont1,cont2,speed){
           var Otext=cont1.text();
