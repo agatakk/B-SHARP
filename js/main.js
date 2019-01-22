85c12dcef7870f44c99eec4c4911e81560333183
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2019-01-22 10:19:38 +0100

    Próby wyeliminowanie błędów zakresu w main.js - 'Otimer is not defined'

--- js/main.js
+++ js/main.js
@@ -83,32 +83,53 @@
 //    }
 //  }
 
+
+
   function changeText(cont1,cont2,speed){
           var Otext=cont1.text();
           var Ocontent=Otext.split("");
           var i=0;
+          
           function show(){
             if(i<Ocontent.length){  
                 cont2.append(Ocontent[i]);
                  i=i+1;
           };
         };
-           var Otimer=setInterval(show,speed);
+        
+          Otimer = setInterval(show,speed); 
+        
+       
         };
-   setTimeout(function(){
+
+    
+
+
+        setTimeout(function(){
           changeText($(".intro"),$(".p2"),35); 
-          clearInterval(Otimer);
+          function clear() {clearInterval(Otimer);}
+        
         },6500);
+        
+
+        const writer = document.getElementById("text")
+
+
 
     setTimeout(function(){
             changeText($(".coming"),$(".p3"),35); 
-            clearInterval(Otimer);
+           function clear(){clearInterval(Otimer)}; 
           },10700);
 
     setTimeout(function(){
             changeText($(".sign-up"),$(".p4"),35); // 35 = the Delay time in milliseconds between strokes.
-            clearInterval(Otimer);
+            function clear(){ clearInterval(Otimer)};
           },11200);
 
-    
-    
+  
+
+   
+
+  
+
+   
