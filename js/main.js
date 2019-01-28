a3fb83c482485d7b84cd52d686b0535747037fcb
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2019-01-28 08:40:54 +0100

    Poprawki pliku index.html - zamiana p na h1 w sekcji #logo, usunięcie zakomentowanego kodu - main.js

--- js/main.js
+++ js/main.js
@@ -1,114 +1,27 @@
-// setTimeout(function(){$("#green").animate({
-//     width: '100vw',
-//     height: '100vh',
-//     top: "0",
-//     left:"0",
-//     borderRadius:'0',
-// },1500)}, 5000);
+  function changeText(cont1, cont2, speed) {
+    var Otext = cont1.text();
+    var Ocontent = Otext.split("");
+    var i = 0;
 
+    function show() {
+      if (i < Ocontent.length) {
+        cont2.append(Ocontent[i]);
+        i = i + 1;
+      };
+    };
+    var Otimer = setInterval(show, speed);
+  };
+  setTimeout(function () {
+    changeText($(".intro"), $(".p2"), 35);
+    clearInterval(Otimer);
+  }, 6500);
 
+  setTimeout(function () {
+    changeText($(".coming"), $(".p3"), 35);
+    clearInterval(Otimer);
+  }, 10700);
 
-// setTimeout(function(){$("#par").animate({
-//     padding: '20px',
-//     // marginLeft: '-90vh',
-//   },1500)},5000);
-
-
-
-// const mql = window.matchMedia("screen and (max-width: 1024px)");
-
-// const mql1 =  window.matchMedia('screen and (max-width: 768px)');
-
-// function moveLogo (mql){
-//   if (mql.matches){
-//     setTimeout(function(){$("#par").animate({
-//          marginLeft: '-40vh',
-          
-//      },1050)},5000);
-//      console.log("działa");
-//   }
-//   else if (mql1.matches){
-//     setTimeout(function(){$("#par").animate({
-//          marginLeft: '-350vh',
-//            },1450)},5000);
-//   }
-//   else{
-//     setTimeout(function(){$("#par").animate({
-//       padding: '20px',
-//       marginLeft: '-90vh',
-//     },1500)},5000);
-//   }
-// }
-// moveLogo(mql);
-// mql.addListener(moveLogo);
-// mql1.addListener(moveLogo);
-  
-// let viewportMobile = window.matchMedia("screen and (max-width: 767px");
-// let viewportMobileLarge = window.matchMedia("screen and (max-width: 1024px");
-
-// if(viewportMobile.matches){
-//     setTimeout(function(){$("#par").animate({
-//         marginLeft: '-350vh',
-//       },1450)},5000);
-// }
-
-// if(viewportMobileLarge.matches){
-//   setTimeout(function(){$("#par").animate({
-//     marginLeft: '-40vh',
-    
-//   },1050)},5000);
-// }
-// const mql = window.matchMedia('screen and (max-width: 1024px)')
-
-// const mqls = [
-//   window.matchMedia("(max-width: 1024px)"),
-//   window.matchMedia("(max-width: 768px)"),
-// ]
-//  function moveLogo(mql){
-//    if(mqls[0].matches){
-//     setTimeout(function(){$("#par").animate({
-//        marginLeft: '-40vh',
-//          },1050)},5000);
-//    }
-//    else if(mqls[1].matches){
-//     setTimeout(function(){$("#par").animate({
-//          marginLeft: '-350vh',
-//       },1450)},5000);
-//    }
-//    else{
-//     setTimeout(function(){$("#par").animate({
-//          padding: '20px',
-//           marginLeft: '-90vh',
-//       },1500)},5000);
-//    }
-//  }
-
-  function changeText(cont1,cont2,speed){
-          var Otext=cont1.text();
-          var Ocontent=Otext.split("");
-          var i=0;
-          function show(){
-            if(i<Ocontent.length){  
-                cont2.append(Ocontent[i]);
-                 i=i+1;
-          };
-        };
-           var Otimer=setInterval(show,speed);
-        };
-   setTimeout(function(){
-          changeText($(".intro"),$(".p2"),35); 
-          clearInterval(Otimer);
-        },6500);
-
-    setTimeout(function(){
-            changeText($(".coming"),$(".p3"),35); 
-            clearInterval(Otimer);
-          },10700);
-
-    setTimeout(function(){
-            changeText($(".sign-up"),$(".p4"),35); // 35 = the Delay time in milliseconds between strokes.
-            clearInterval(Otimer);
-          },11200);
-
-    
-    
+  setTimeout(function () {
+    changeText($(".sign-up"), $(".p4"), 35); // 35 = the Delay time in milliseconds between strokes.
+    clearInterval(Otimer);
+  }, 11200);
